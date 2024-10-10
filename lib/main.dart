84834a2fa4c2a6a4558cc12650350375216d6904
yayediop <mamyta2000@outlook.fr>
2024-10-10 20:30:58 +0000

    add: icons by adding rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,52 +26,116 @@ class MyApp extends StatelessWidget {
 class BizzCard extends StatelessWidget {
   const BizzCard({super.key});
 
+  // SVG strings for icons
+  final String nameIcon = '''
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+  </svg>
+  ''';
+
+  final String emailIcon = '''
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+</svg>
+  ''';
+
+  final String phoneIcon = '''
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+</svg>
+  ''';
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
         child: Card(
-      margin: EdgeInsets.all(20),
-      color: Colors.blueAccent,
+      margin: const EdgeInsets.all(20),
+      color: const Color.fromARGB(255, 228, 231, 236),
       elevation: 8,
       shadowColor: Colors.black,
       child: SizedBox(
         height: 400,
+        width: 500,
         child: Padding(
-          padding: EdgeInsets.all(30.0),
+          padding: const EdgeInsets.all(30.0),
           child: Column(
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 75,
                 backgroundImage: AssetImage('../assets/pp.webp'),
               ),
-              SizedBox(height: 30),
-              Text(
-                "Yaye Fatou Diop",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.string(
+                    nameIcon,
+                    height: 20,
+                    width: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Yaye Fatou Diop",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              Text(
-                "64 years old",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.string(
+                    nameIcon,
+                    height: 20,
+                    width: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "24 years old",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              Text(
-                "yaye.fatou_diop0000@gmail.com",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.string(
+                    emailIcon,
+                    height: 20,
+                    width: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "yayefatoudiop2000@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              Text(
-                "+221 78 156 23 49",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.string(
+                    phoneIcon,
+                    height: 20,
+                    width: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "+221 78 156 23 49",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
